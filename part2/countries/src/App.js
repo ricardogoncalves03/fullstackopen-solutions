a8970c4c5578dab1906b6509c6ac8f1e1c4d5a2b
ricardojtgoncalves <ricardogoncalves03@gmail.com>
2022-02-12 00:23:20 +0000

    Work on 2.12* Data for countries, step1. First draft concluded

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import useFetch from './useFetch'
+import Filter from './components/Filter';
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-  //const { countires } = useFetch('https://restcountries.com/v2/all');
+  const [filterCountries, setFilterCountries] = useState('');
 
   useEffect(() => {
     console.log('use effect ran');
@@ -13,16 +13,54 @@ const App = () => {
       .then((response) => setCountries(response.data))
   }, []);
 
-  console.log("Countries", countries[0]);
-  //console.log(countries)
-  const pestans = useFetch('https://restcountries.com/v2/all');
-  pestans.map(ya => ya.name)
-  return (
-    <div>
-      <p>OI</p>
-      { countries.map(country => <p>{country.name}</p>) }
-    </div>
-  );
+  const handleFilterCountry = (event) => {
+    setFilterCountries(event.target.value);
+  };
+
+  const countriesToShow = filterCountries 
+    ? countries.filter(country => country.name.toLowerCase().includes(filterCountries))
+    : countries;
+
+  if (countriesToShow.length >= 10) {
+    return (
+      <div>
+        <Filter value={filterCountries} onChange={handleFilterCountry} />
+        <br></br>
+        Too many matches
+      </div>
+    );
+  } else if (countriesToShow.length === 1) {
+    return (
+      <div>
+        <Filter value={filterCountries} onChange={handleFilterCountry} />
+        <br></br>
+        <h1>{countriesToShow.map(country => country.name)}</h1>
+        <p>capital - {countriesToShow.map(country => country.capital)}</p>
+        <p>area - {countriesToShow.map(country => country.area)}</p>
+        <h2>languages</h2>
+        <ul>
+          <li>{countriesToShow.map(country => country.languages.map(
+            language => <li key={language.name}>{language.name}</li>))}</li>
+        </ul>
+        <img src={countriesToShow.map(country => country.flag)} alt="Country flag"></img>
+      </div>
+    );
+  } else {
+      return (
+        <div>
+          <Filter value={filterCountries} onChange={handleFilterCountry} />
+          <div>
+            {
+                countriesToShow.map(country => (
+                  <p key={country.name}>
+                    {country.name} <button>show</button>
+                  </p>
+                ))
+            }
+          </div>
+        </div>
+      );
+  } 
 };
 
 export default App;
---
+++ part2/countries/src/components/Filter.js
@@ -0,0 +1,11 @@
+const Filter = (props) => {
+
+  return (
+    <div>
+      find countries <input value={props.value}
+      onChange={props.onChange} />
+    </div>
+  );
+}
+
+export default Filter;
